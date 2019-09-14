5d11a04e91ebd7d399051e35e06c8808b5b0b06c
mnreyes1 <mnreyes1@uc.cl>
2019-09-14 12:18:46 -0300

    saved config files

--- copy_config_files.sh
+++
@@ -1,7 +0,0 @@
-echo "(i3) copying config"
-cp ~/.config/i3/config i3_conf/config
-cd ~/.config/ranger/
-for file in *; do
-  echo "(ranger) copying $file"
-  cp "$file" ~/Repos/i3/ranger_conf/"$file"
-done
